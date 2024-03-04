785036c1a0a1bd4a1b3bcf2d3e067e0468a5f72b
Toby Chow <toby_chow@hotmail.com>
2024-03-04 01:00:28 -0500

    use card component for todo

--- src/features/todo/Todo.js
+++ src/features/todo/Todo.js
@@ -3,18 +3,19 @@ import { TasksProvider } from './TasksContext';
 import AddTask from './AddTask/AddTask';
 import TaskList from './TaskList/TaskList';
 import StatusTab from './StatusTab/StatusTab';
+import Card from '../../components/Card/Card';
 import styles from './Todo.module.css';
 
 export default function Todo() {
     const [taskStatus, setTaskStatus] = useState('');
 
     return (
-        <div className={`${styles.todoContainer} ${styles.card}`}>
+        <Card moduleStyle={styles.todoContainer}>
             <TasksProvider>
                 <StatusTab taskStatus={taskStatus} setTaskStatus={setTaskStatus}/>
                 <AddTask />
                 <TaskList taskStatus={taskStatus}/>
             </TasksProvider>
-        </div>
+        </Card>
     );
 }
